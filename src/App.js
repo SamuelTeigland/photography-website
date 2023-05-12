7631f3067409cfc559c61ee3b9e3e06e9217d770
Samuel Teigland <stsoccer922@gmail.com>
2023-05-12 14:24:00 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -2,9 +2,26 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+
+  const [backendData, setBackendData] = useState([{}]);
+
+  useEffect(() => {
+    fetch('/api').then(
+      response => response.json()
+    ).then(data => {
+      setBackendData(data);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <h1>React App</h1>
+      {(typeof backendData === 'undefined') ? (
+        <p>Loading...</p>
+      ) : (
+        backendData.users.map((user, i) => (
+          <p key={i}>{user}</p>
+        ))
+      )}
     </div>
   );
 }
